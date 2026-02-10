13ef41571a690ed4c61156e7f74b015eca26cdf3
dldb-chamchi <dladkfl49@gmail.com>
2026-02-10 20:55:16 +0900

    feat: controller 기본 세팅

--- src/main/java/com/memoryshade/domain/guardian/controller/GuardianController.java
+++ src/main/java/com/memoryshade/domain/guardian/controller/GuardianController.java
@@ -1,4 +1,11 @@
 package com.memoryshade.domain.guardian.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/guardians")
+@RequiredArgsConstructor
 public class GuardianController {
 }
--- src/main/java/com/memoryshade/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/memoryshade/domain/schedule/controller/ScheduleController.java
@@ -1,4 +1,11 @@
 package com.memoryshade.domain.schedule.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/schedules")
+@RequiredArgsConstructor
 public class ScheduleController {
 }
