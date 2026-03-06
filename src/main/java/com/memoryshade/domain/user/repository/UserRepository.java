3c40b9f8da480c4f0f441318a17cd9521b5d61d8
dldb-chamchi <dladkfl49@gmail.com>
2026-03-06 13:29:18 +0900

    feat: userrepository 메소드 추가

--- src/main/java/com/memoryshade/domain/user/repository/UserRepository.java
+++ src/main/java/com/memoryshade/domain/user/repository/UserRepository.java
@@ -1,7 +1,25 @@
 package com.memoryshade.domain.user.repository;
 
+import com.memoryshade.domain.auth.exception.AuthErrorCode;
 import com.memoryshade.domain.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.memoryshade.global.exception.ExceptionList;
+import org.springframework.data.repository.Repository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
+
+public interface UserRepository extends Repository<User, Long> {
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    User save(User user);
+
+    default User getByPhoneNumber(String phoneNumber) {
+        return findByPhoneNumber(phoneNumber)
+                .orElseThrow(() -> new ExceptionList(AuthErrorCode.USER_NOT_FOUND));
+    }
+
+    default void validateDuplicatePhoneNumber(String phoneNumber) {
+        findByPhoneNumber(phoneNumber)
+                .ifPresent(u -> { throw new ExceptionList(AuthErrorCode.DUPLICATE_PHONE_NUMBER); });
+    }
 }
