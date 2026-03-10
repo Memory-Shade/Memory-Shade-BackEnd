16dfd5d60be896177659656f46418b3266df78cf
dldb-chamchi <dladkfl49@gmail.com>
2026-03-10 17:48:27 +0900

    feat: CRD controller

--- src/main/java/com/memoryshade/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/memoryshade/domain/schedule/controller/ScheduleController.java
@@ -1,11 +1,44 @@
 package com.memoryshade.domain.schedule.controller;
 
+import com.memoryshade.domain.schedule.dto.ScheduleRequestDto;
+import com.memoryshade.domain.schedule.dto.ScheduleResponseDto;
+import com.memoryshade.domain.schedule.service.ScheduleService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/schedules")
+@RequestMapping("/api/schedules")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping
+    public ResponseEntity<ScheduleResponseDto> createSchedule(
+            @AuthenticationPrincipal Long loginUserId,
+            @Valid @RequestBody ScheduleRequestDto request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.create(loginUserId, request));
+    }
+
+    @PutMapping("/{scheduleId}")
+    public ResponseEntity<ScheduleResponseDto> updateSchedule(
+            @AuthenticationPrincipal Long loginUserId,
+            @PathVariable Long scheduleId,
+            @Valid @RequestBody ScheduleRequestDto request) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.update(loginUserId, scheduleId, request));
+    }
+
+    @DeleteMapping("/{scheduleId}")
+    public ResponseEntity<Void> deleteSchedule(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable Long scheduleId) {
+        scheduleService.delete(userId, scheduleId);
+        return ResponseEntity.noContent().build();
+    }
 }
