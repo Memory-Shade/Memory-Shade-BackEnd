c543daa2cbd81a48d6c790784f198950f634d195
dldb-chamchi <dladkfl49@gmail.com>
2026-03-10 17:49:15 +0900

    style: snake_case

--- src/main/java/com/memoryshade/domain/auth/dto/LoginRequestDto.java
+++ src/main/java/com/memoryshade/domain/auth/dto/LoginRequestDto.java
@@ -1,8 +1,13 @@
 package com.memoryshade.domain.auth.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record LoginRequestDto(
+
+        @JsonProperty("phone_number")
         @NotBlank(message = "전화번호를 입력해주세요")
+        @Pattern(regexp = "^01[0-9]{8,9}$", message = "전화번호 형식이 올바르지 않습니다")
         String phoneNumber,
 
         @NotBlank(message = "비밀번호를 입력해주세요")
