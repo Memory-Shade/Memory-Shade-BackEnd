7f077b285af95e492ffec65b877d0430dd548aad
dldb-chamchi <dladkfl49@gmail.com>
2026-03-10 17:47:47 +0900

    feat: CRD 메소드

--- src/main/java/com/memoryshade/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/memoryshade/domain/schedule/repository/ScheduleRepository.java
@@ -1,9 +1,21 @@
 package com.memoryshade.domain.schedule.repository;
 
+import com.memoryshade.domain.schedule.exception.ScheduleErrorCode;
 import com.memoryshade.domain.schedule.model.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.memoryshade.global.exception.ExceptionList;
+import org.springframework.data.repository.Repository;
 
-@Repository
-public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+import java.util.Optional;
+
+public interface ScheduleRepository extends Repository<Schedule, Long> {
+
+    Schedule save(Schedule schedule);
+    void delete(Schedule schedule);
+
+    Optional<Schedule> findByScheduleIdAndUser_UserId(Long scheduleId, Long userId);
+
+    default Schedule getByScheduleIdAndUserId(Long scheduleId, Long userId) {
+        return findByScheduleIdAndUser_UserId(scheduleId, userId)
+                .orElseThrow(() -> new ExceptionList(ScheduleErrorCode.SCHEDULE_NOT_FOUND));
+    }
 }
