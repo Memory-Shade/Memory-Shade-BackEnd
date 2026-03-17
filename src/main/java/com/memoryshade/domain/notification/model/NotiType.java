d0c592a94e049530cd09a4ee9015a4ec93a2987d
dldb-chamchi <dladkfl49@gmail.com>
2026-03-17 22:41:09 +0900

    style: noti 유형 수정

--- src/main/java/com/memoryshade/domain/notification/model/NotiType.java
+++ src/main/java/com/memoryshade/domain/notification/model/NotiType.java
@@ -2,8 +2,7 @@ package com.memoryshade.domain.notification.model;
 
 public enum NotiType {
     SCHEDULE, // 사용자 일정 알람
-    DIARY_WRITTEN, // 사용자가 일기 작성 완료 (보호자 수신)
     DIARY_SHARED, // 보호자에게 일기 공유됨 (보호자 수신)
     GPS_ALERT, // 사용자 안전 반경 이탈 (보호자 수신)
-    EMOTION_REPORT // 감정 분석 리포트 생성
+    EMOTION_REPORT // 감정 분석
 }
