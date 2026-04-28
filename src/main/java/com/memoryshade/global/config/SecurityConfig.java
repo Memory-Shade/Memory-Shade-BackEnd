720eaabff851ea4fb8feee706869683e5fe79e7c
dldb-chamchi <dladkfl49@gmail.com>
2026-04-28 19:30:37 +0900

    chore: 환경설정

--- src/main/java/com/memoryshade/global/config/SecurityConfig.java
+++ src/main/java/com/memoryshade/global/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
